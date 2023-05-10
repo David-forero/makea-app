f70c9a8c028b7f458779365bee087bd0bea3087d
David Forero <54685136+David-forero@users.noreply.github.com>
2023-05-10 12:25:22 -0400

    modulo de auth listo 🔥

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,10 +2,12 @@ import HomeScreen from './HomeScreen';
 import DetailsScreen from './DetailsScreen';
 import OrderScreen from './OrderScreen'
 import BasketScreen from './BasketScreen';
+import AuthScreen from './AuthScreen';
 
 export {
     HomeScreen,
     DetailsScreen,
     OrderScreen,
-    BasketScreen
+    BasketScreen,
+    AuthScreen
 }
